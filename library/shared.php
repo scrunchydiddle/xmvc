703a1155637d2c23725a1ffb3598ff34c2efae4c
scrunchydiddle <neorhapsody66@gmail.com>
2010-12-22 09:54:19 +0800

    add shared.php

--- library/shared.php
+++ library/shared.php
@@ -21,9 +21,66 @@ function removeMagicQuotes() {
         $_GET   = stripSlashesDeep($_GET);
         $_POST  = stripSlashesDeep($_POST);
         $_COOKIE  = stripSlashesDeep($_COOKIE);
+    
     }
 }
 
 function unregisterGlobals() {
-
+    if (ini_get('register_globals')) {
+        $array = array('_SESSION', '_POST', '_GET', '_COOKIE', '_REQUEST', '_SERVER', '_ENV', '_FILES');
+        foreach ($array as $value) {
+            foreach ($GLOBALS[$value] as $key => $var) {
+                if ($var === $GLOBALS[$key]) {
+                    unset($GLOBALS[$key]);
+                }
+            }
+        }
+    }
 }
+
+/** Main Call Function **/
+
+function callHook() {
+    global $url;
+
+    $urlArray = array();
+    $urlArray = explode("/",$url);
+
+    $controller = $urlArray[0];
+    array_shift($urlArray);
+    $action = $urlArray[0];
+    array_shift($urlArray);
+    $queryString = $urlArray;
+
+    $controllerName = $controller;
+    $controller = ucwords($controller);
+    $model = rtrim($controller, 's');
+    $controller .= 'Controller';
+    $dispatch = new $controller($model,$controllerName,$action);
+
+    if ((int)method_exists($controller, $action)) {
+        call_user_func_array(array($dispatch,$action),$queryString);
+    } else {
+        /* Error Generation Code Here */
+    }
+}
+
+/** Autoload any classes that are required **/
+
+function __autoload($className) {
+    if (file_exists(ROOT . DS . 'library' . DS . strtolower($className) . '.class.php')) {
+        require_once(ROOT . DS . 'library' . DS . strtolower($className) . '.class.php');
+    } else if (file_exists(ROOT . DS . 'application' . DS . 'controllers' . DS . strtolower($className) . '.php')) {
+        require_once(ROOT . DS . 'application' . DS . 'controllers' . DS . strtolower($className) . '.php');
+    } else if (file_exists(ROOT . DS . 'application' . DS . 'models' . DS . strtolower($className) . '.php')) {
+        require_once(ROOT . DS . 'application' . DS . 'models' . DS . strtolower($className) . '.php');
+    } else {
+        /* Error Generation Code Here */
+    }
+}
+
+setReporting();
+removeMagicQuotes();
+unregisterGlobals();
+callHook();
+
